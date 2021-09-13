59122d6bcaaa27069528c5c9af50f46114e9ae02
Eliezer Romero <eliezerfot123@gmail.com>
2021-09-12 23:53:18 -0400

    configuracion inicial para proyecto

--- src/server.js
+++ src/server.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const path = require('path');
 
+//Inicializacioness
 const app = express();
+
+//settings 
+app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname +'views'));
+//Middleware
+
+app.use(express.urlencoded({extended: false}));
+
+// Global variables
+
+//router
+
+app.get('/', (req, res) => {
+    res.send('Hello word');
+});
+
+//Staticfiles
+app.set(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
